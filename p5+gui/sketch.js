6695bbe7b74f33066d7cab3c41675b4ed749cf31
Nicole Wei <weiyichen2004@gmail.com>
2025-11-14 16:02:18 -0600

    added separate jquery page

--- p5+gui/sketch.js
+++ p5+gui/sketch.js
@@ -1,8 +1,16 @@
 // var slider = document.getElementById
 
 $(document).ready(function () {
-  $('.btn-hide').click(function () {
-    $(".p-clickhide").hide(1000);
+  $(".btn-hide").click(function () {
+    $(".p-clickhide").toggle(1000);
     alert("ww");
   });
+  $(".btn-hide").hover(
+    function(){
+      $(this).css("background-color", "#1F1F1F");
+    },
+    function(){
+      $(this).css("background-color", "black");
+    }
+  );
 });
