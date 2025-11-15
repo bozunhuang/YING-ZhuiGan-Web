3f015a8c82da91c0c7672a5c3c4eb660febcf9e5
Nicole Wei <weiyichen2004@gmail.com>
2025-11-15 10:02:43 -0600

    GUI: slider, text, buttons

---
+++ p5+gui/jquery-sketch.js
@@ -0,0 +1,65 @@
+// var slider = document.getElementById
+
+$(document).ready(function () {
+  $(".btn-hide").click(function () {
+    $(".p-clickhide").toggle(1000);
+  });
+
+  $(".btn").hover(
+    function () {
+      $(this).css("background-color", "#1F1F1F");
+    },
+    function () {
+      $(this).css("background-color", "black");
+    }
+  );
+
+  $(".btn-fade").click(function () {
+    $(".p-clickfade").fadeTo("fast", 0.4);
+  });
+
+  $(".btn-slide").click(function () {
+    $(".p-clickslide").slideToggle("fast");
+  });
+
+  // $(".btn-animate").click(function () {
+  //   $(".p-clickanimate").animate({
+  //     left: '60px',
+  //     fontSize: '2rem'
+  //   }, "fast");
+  // });
+
+  $(".btn-animate").click(function () {
+    $(".p-clickanimate").animate(
+      {
+        left: "60px",
+      },
+      "fast"
+    );
+    $(".p-clickanimate").animate(
+      {
+        fontSize: "2rem",
+      },
+      "fast"
+    );
+  });
+
+
+  //html methods
+  $(".btn-gettext").click(function(){
+    $(".p-showtext").text(function(i, origText){
+        return $(".hidden").text();
+    })
+  });
+
+  $(".btn-addtext").click(function(){
+    var txt = document.createElement("p");
+    txt.innerHTML = "This is a new p element";
+    $(".p-addtext").append("Hello ");
+    $(".p-addtext").after(txt);
+  });
+
+  $(".btn-removetext").click(function(){
+    $(".div-add").empty();
+  });
+});
--- p5+gui/sketch.js
+++ p5+gui/sketch.js
@@ -1,65 +1,104 @@
-// var slider = document.getElementById
+// var slider = $("#slider");
 
-$(document).ready(function () {
-  $(".btn-hide").click(function () {
-    $(".p-clickhide").toggle(1000);
-  });
 
-  $(".btn").hover(
-    function () {
-      $(this).css("background-color", "#1F1F1F");
-    },
-    function () {
-      $(this).css("background-color", "black");
+// function setup(){
+//     var c = createCanvas(500, 500);
+//     c.parent("canvasWrapper");
+// }
+
+// function draw(){
+//     background("#000000");
+//     noStroke();
+//     fill("#0000ff");
+//     circle(width/2,height/2,slider.val());
+// }
+
+//slider
+
+let sliderSketch = function(p){
+    let slider;
+
+    p.setup = function(){
+        let c = p.createCanvas(500, 500);
+        c.parent("canvasWrapper");
+
+        slider = $("#slider");
+    };
+
+    p.draw = function(){
+        p.background(0);
+        p.fill("#0000FF");
+        p.circle(width/2, height/2, slider.val());
+    };
+};
+
+new p5(sliderSketch);
+
+
+//buttons
+let buttonSketch = function(p){
+    let btn;
+    let bgColor;
+    p.setup = function(){
+        let c2 = p.createCanvas(500, 500);
+        c2.parent("sec3CanvasWrapper");
+        btn = $("#btn-bg");
+        bgColor = 100;
+    };
+    p.draw = function(){ 
+        p.background(0);
+        p.fill(bgColor);
+        p.circle(width/2, height/2, 400);
+    };
+    p.changeBGColor = function(){
+        bgColor+=random(-50, 50);
     }
-  );
+};
 
-  $(".btn-fade").click(function () {
-    $(".p-clickfade").fadeTo("fast", 0.4);
-  });
+let myButtonSketch = new p5(buttonSketch);
 
-  $(".btn-slide").click(function () {
-    $(".p-clickslide").slideToggle("fast");
-  });
-
-  // $(".btn-animate").click(function () {
-  //   $(".p-clickanimate").animate({
-  //     left: '60px',
-  //     fontSize: '2rem'
-  //   }, "fast");
-  // });
-
-  $(".btn-animate").click(function () {
-    $(".p-clickanimate").animate(
-      {
-        left: "60px",
-      },
-      "fast"
-    );
-    $(".p-clickanimate").animate(
-      {
-        fontSize: "2rem",
-      },
-      "fast"
-    );
-  });
-
-
-  //html methods
-  $(".btn-gettext").click(function(){
-    $(".p-showtext").text(function(i, origText){
-          return $(".hidden").text();
-    })
-  });
-
-  $(".btn-addtext").click(function(){
-    var txt = document.createElement("p");
-    txt.innerHTML = "This is a new p element";
-    $(".p-addtext").append("Hello ");
-    $(".p-addtext").after(txt);
-  });
-
-  $(".btn-removetext").click(function(){
-    $(".div-add").empty();
-  });
+$("#btn-bg").click(function(){
+    myButtonSketch.changeBGColor();
 });
+
+
+
+//text field
+var textInput = $("#text-field");
+
+function setup(){
+    var c1 = createCanvas(500, 500);
+    c1.parent("sec2CanvasWrapper");
+}
+
+function draw(){
+    background(0);
+    fill("#0000ff");
+    textSize(80);
+    textAlign(CENTER, CENTER);
+    text(textInput.val(), width/2, height/2);
+}
+
+new p5(textInputSketch);
+
+
+// let textInputSketch = function(p){
+//     let textInput;
+//     p.setup = function(){
+//         let c1 = p.createCanvas(500, 500);
+//         c1.parent("sec2CanvasWrapper");
+//         textInput = $("#text-field");
+//     }
+//     p.draw = function(){
+//         p.background(0);
+//         p.fill("#0000ff");
+//         p.textSize(80);
+//         p.textAlign(CENTER, CENTER);
+//         p.text(textInput.val(), width/2, height/2);
+//     }
+
+// }
+
+
+
+
