771edf11eefbd575d67f45e9f1cdd9030fb2d6b5
Nicole Wei <weiyichen2004@gmail.com>
2025-11-26 13:56:34 -0600

    scrollTrigger learning update

--- p5+gui/gsap-script.js
+++ p5+gui/gsap-script.js
@@ -1,4 +1,31 @@
-gsap.to(".box", {
+// import { gsap } from "gsap/dist/gsap";
+    
+// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+
+gsap.to(".group-from-to .box", {
     rotation: 360,
     duration: 5
+})
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.to(".box-d", {
+    scrollTrigger:".box-d",
+    x: 400,
+    rotation: 360, 
+    duration: 2
+})
+
+gsap.to("#box-toggle-action", {
+    scrollTrigger: {
+        trigger: "#box-toggle-action",
+        toggleActions: "restart pause resume none", 
+        start: "top center",
+        markers: true,
+    },
+    x: 400,
+    rotation: 360, 
+    duration: 2
 })
